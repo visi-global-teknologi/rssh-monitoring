42fffa507e33c5b468ac2ffb3841dedf1ea10e30
la790x <la790x@gmail.com>
2023-05-19 15:12:18 +0700

    fix dikit

--- app/Actions/Api/Private/Datatable/CronLog/Handler.php
+++ app/Actions/Api/Private/Datatable/CronLog/Handler.php
@@ -10,7 +10,7 @@ class Handler
 {
     public function handle(Request $request)
     {
-        $query = CronLog::query()->with(['rssh_connection.device.client'])->groupBy('rssh_connection_id')->latest();
+        $query = CronLog::query()->latest()->with(['rssh_connection.device.client'])->groupBy('rssh_connection_id');
 
         return DataTables::of($query)
             ->addColumn('created_at_human_formatted', function ($row) {
