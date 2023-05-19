8551deaff39662115aa20490b7af99ff2235a25b
la790x <la790x@gmail.com>
2023-05-19 17:40:57 +0700

    add migration cron_log_view

---
+++ app/Models/CronLogView.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class CronLogView
+ * 
+ * @property int $id
+ * @property string $log
+ * @property string $is_error
+ * @property string $file_name
+ * @property int $rssh_connection_id
+ * @property string $rssh_connection_server_port
+ * @property string $device_name
+ * @property string $device_unique_code
+ * @property string $device_active_status
+ * @property string $client_name
+ *
+ * @package App\Models
+ */
+class CronLogView extends Model
+{
+	protected $table = 'cron_log_view';
+	public $incrementing = false;
+	public $timestamps = false;
+
+	protected $casts = [
+		'id' => 'int',
+		'rssh_connection_id' => 'int'
+	];
+
+	protected $fillable = [
+		'id',
+		'log',
+		'is_error',
+		'file_name',
+		'rssh_connection_id',
+		'rssh_connection_server_port',
+		'device_name',
+		'device_unique_code',
+		'device_active_status',
+		'client_name'
+	];
+}
