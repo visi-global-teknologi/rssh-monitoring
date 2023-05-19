7e94c53aa15957d1dcfe6a323cd13295d28a61c1
la790x <la790x@gmail.com>
2023-05-19 15:53:38 +0700

    datatable cron log

--- app/Actions/Api/Private/Datatable/CronLog/Handler.php
+++ app/Actions/Api/Private/Datatable/CronLog/Handler.php
@@ -11,14 +11,8 @@ class Handler
 {
     public function handle(Request $request)
     {
-        return DataTables::query(
-            DB::table('cron_logs')
-                ->orderBy('created_at', 'desc')
-			    ->groupBy('rssh_connection_id')
-        )
-        ->toJson();
-        // $query = CronLog::query()->with(['rssh_connection.device.client'])->latest('created_at')->groupBy('rssh_connection_id');
+        $query = CronLog::query()->with(['rssh_connection.device.client']);
 
-        // return DataTables::of($query)->toJson();
+        return DataTables::of($query)->toJson();
     }
 }
