bc4b21442409d445035f3166060e0fddb210b234
la790x <la790x@gmail.com>
2023-05-22 11:11:12 +0700

    rev titile

--- resources/views/layouts/skote/master.blade.php
+++ resources/views/layouts/skote/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title> @yield('title') | PM</title>
+    <title> @yield('title')</title>
     <meta charset="utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
