005543822f228c13f7fd0aad35704463443c4c18
la790x <la790x@gmail.com>
2023-05-23 22:59:39 +0700

    fix dikit

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
 });
 Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('devices', \App\Http\Controllers\DeviceController::class);
-Route::resource('clients', \App\Http\Controllers\ClientController::class);
-Route::resource('cron-logs', \App\Http\Controllers\CronLogController::class);
-Route::resource('rssh-logs', \App\Http\Controllers\RsshLogController::class);
-Route::resource('ping-servers', \App\Http\Controllers\PingServerController::class);
-Route::resource('rssh-connections', \App\Http\Controllers\RsshConnectionController::class);
+Route::resource('devices', \App\Http\Controllers\DeviceController::class)->middleware(['auth']);
+Route::resource('clients', \App\Http\Controllers\ClientController::class)->middleware(['auth']);
+Route::resource('cron-logs', \App\Http\Controllers\CronLogController::class)->middleware(['auth']);
+Route::resource('rssh-logs', \App\Http\Controllers\RsshLogController::class)->middleware(['auth']);
+Route::resource('ping-servers', \App\Http\Controllers\PingServerController::class)->middleware(['auth']);
+Route::resource('rssh-connections', \App\Http\Controllers\RsshConnectionController::class)->middleware(['auth']);
